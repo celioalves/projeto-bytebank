7f0602ab75f35c4f7e15c0ba1bf4cbef7dde0bc7
celioalves <craftsman7@gmail.com>
2023-03-15 08:48:31 -0300

    Alteração da quebra de idade e arquivo main

--- codigo/main.py
+++ codigo/main.py
@@ -1,5 +1,8 @@
 from codigo.bytebank import Funcionario
 
-lucas = Funcionario("Lucas Silveira", "13/03/2000", 1000)
 
-print(lucas.idade())
+def teste_idade():
+    funcionario_teste = Funcionario('Teste', '13/03/1990', 1111)
+    print(f'Teste = {funcionario_teste.idade()}')
+
+teste_idade()
