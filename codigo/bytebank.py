529e4bc4e6b8ed965a438363e8a610e30763b562
celioalves <craftsman7@gmail.com>
2023-03-16 10:50:18 -0300

    Adicionada nova função de cálculo do bonus e Exception

--- codigo/bytebank.py
+++ codigo/bytebank.py
@@ -37,7 +37,7 @@ class Funcionario:
     def calcular_bonus(self):
         valor = self._salario * 0.1
         if valor > 1000:
-            valor = 0
+            raise Exception('O Salário é elevado para os parâmetros de bonificação')
         return valor
 
     def __str__(self):
